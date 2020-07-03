26a6c4c74cffe2165f4303d5eee7f448ebd246bc
Someswara Rao Palakolanu <someswararao.palakolanu@imaginea.com>
2020-07-03 22:35:35 +0530

    added max listeners

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+require('events').EventEmitter.defaultMaxListeners = 15;
 
 app.use(express.static(__dirname));
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build','index.html'))
+    res.sendFile(path.resolve(__dirname,'index.html'))
 });
 
 app.listen(port);
