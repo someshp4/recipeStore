7c7061301ff432a2217f979691309e5918a92f90
Someswara Rao Palakolanu <someswararao.palakolanu@imaginea.com>
2020-07-05 16:51:10 +0530

    fixed server.js

--- server.js
+++ server.js
@@ -4,10 +4,19 @@ const port = process.env.PORT || 8080;
 const app = express();
 require('events').EventEmitter.defaultMaxListeners = 15;
 
-app.use(express.static(__dirname));
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'index.html'))
+// app.use(express.static(__dirname));
+// app.get('/', (req, res) => {
+//     console.log("req",req);
+//     console.log("dirname", __dirname);
+//     console.log("path",path.resolve(__dirname, 'public/index.html'));
+//     res.sendFile(path.resolve(__dirname, 'build/index.html'));
+// });
+
+app.use(express.static(path.join(__dirname, 'build')));
+//-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port);
-console.log(`Server started on port: ${port}`)
+console.log(`Server started on port: ${port}`);
