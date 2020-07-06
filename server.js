2e8b08993ca17b6785a027d224e88c76f213f93f
Someswara Rao Palakolanu <someswararao.palakolanu@imaginea.com>
2020-07-06 11:35:13 +0530

    added default listeners

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+require('events').EventEmitter.defaultMaxListeners = 15;
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) {
