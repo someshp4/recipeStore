ba20442579ef35157af22e15efa1945d732723a0
Someswara Rao Palakolanu <someswararao.palakolanu@imaginea.com>
2020-07-06 09:06:16 +0530

    removed unwanted code

--- server.js
+++ server.js
@@ -2,18 +2,8 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-require('events').EventEmitter.defaultMaxListeners = 15;
-
-// app.use(express.static(__dirname));
-// app.get('/', (req, res) => {
-//     console.log("req",req);
-//     console.log("dirname", __dirname);
-//     console.log("path",path.resolve(__dirname, 'public/index.html'));
-//     res.sendFile(path.resolve(__dirname, 'build/index.html'));
-// });
 
 app.use(express.static(path.join(__dirname, 'build')));
-//-app.get('/', function (req, res) {
 app.get('/*', function (req, res) {
    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
